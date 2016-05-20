26ce684593a215d5367f7c33c0b78cbb5bb10973
Joshua Smith <joshsmith01@me.com>
2016-05-20 15:56:03 -0700

    Check to see if the user is logged into the site. If the user is not logged in then redirect him/her to the login page.

--- src/layouts/default.php
+++ src/layouts/default.php
@@ -5,7 +5,15 @@
  * Date: 5/19/16
  * Time: 21:53
  */
+// Start the session all pages first. A session has to be present *before* I check to see that it is set with the next block of code.
 session_start();
+
+// Check to see if the user is actually logged in and if the user is on a page other than the the login page.
+// If the user is not logged in and the user in not on the log in page, then redirect them to the log in page.
+if ( ( ! isset( $_SESSION['logged_in'] ) ) && ( $_SERVER['REQUEST_URI'] != "/login.php" ) ) { //if login in session is not set
+	header( "Location: /login.php" );
+	echo $_SESSION['logged_in'];
+}
 ?>
 
 {{!-- This is the base layout for your project, and will be used on every page. --}}
